3c0bc872785c38bdd1a12aae16bb96dd18400bb0
laiyuancai <laiyuancai@163.com>
2018-04-11 22:45:48 +0800

    blogv1.3-修改样式

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,17 +1,27 @@
 from django import template
-from ..models import Post, Category
+from django.db.models.aggregates import Count
+
+from ..models import Post, Category, Tag
 
 register = template.Library()
 
+
 @register.simple_tag
 def get_recent_posts(num=5):
-    return Post.objects.all().order_by('-created_time')[:num]
-    
+    return Post.objects.all()[:num]
+
+
 @register.simple_tag
 def archives():
     return Post.objects.dates('created_time', 'month', order='DESC')
-    
+
+
 @register.simple_tag
 def get_categories():
-    # 别忘了在顶部引入 Category 类
-    return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+    
+    
+@register.simple_tag
+def get_tags():
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+    
