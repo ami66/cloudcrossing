e1cf33b4a9bba232c614319ff33e1826c40b8987
e <laiyuancai@163.com>
2018-04-18 15:45:29 +0800

    blogv2.1-增加发送重置密码邮件

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from blog.feeds import AllPostsRssFeed
 
 from users import views
@@ -12,8 +13,38 @@ urlpatterns = [
     url(r'', include('blog.urls')),
     url(r'', include('comments.urls')),
     url(r'^all/rss/$', AllPostsRssFeed(), name='rss'),
+    
+    #注册
     url(r'^users/', include('users.urls')),
-    url(r'^users/', include('django.contrib.auth.urls')),
+    
+    #登录、注销
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),\
+
+    #重置密码
+    url(r'^reset/$',
+        auth_views.PasswordResetView.as_view(
+            template_name='password_reset.html',
+            email_template_name='password_reset_email.html',
+            subject_template_name='password_reset_subject.txt'
+        ),
+        name='password_reset'),
+    url(r'^reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    url(r'^reset/complete/$',
+        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+        name='password_reset_complete'),
+    
+    #修改密码
+    url(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+        name='password_change'),
+    url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+        name='password_change_done'),    
+        
     url(r'^$', views.index, name='index'),
     url(r'mdeditor/', include('mdeditor.urls')),
     url(r'^search/', include('haystack.urls')),
