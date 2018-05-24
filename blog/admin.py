2cd05a1f82e44f1190b9485c6272dbfe5a95b685
e <laiyuancai@163.com>
2018-05-24 11:14:02 +0800

    blogv2.93-后台增加文章查找

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,9 @@ from .models import Post, Category, Tag
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
     
+    list_filter = ('title','category',)
+    search_fields = ('title',)
+    
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
